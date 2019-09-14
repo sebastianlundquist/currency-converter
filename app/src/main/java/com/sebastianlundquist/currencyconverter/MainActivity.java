a76b26cf17c612e7aa5e927ac80a69d71318b1b0
Sebastian Lundqvist <seblu507@student.liu.se>
2019-09-14 12:58:50 +0200

    Initial commit

---
+++ app/src/main/java/com/sebastianlundquist/currencyconverter/MainActivity.java
@@ -0,0 +1,61 @@
+package com.sebastianlundquist.currencyconverter;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convertCurrency(View view) {
+        EditText editText = findViewById(R.id.inputCurrency);
+        double currencyAmount;
+        try {
+            currencyAmount = Double.parseDouble(editText.getText().toString());
+            Toast.makeText(this, "£" + String.format("%.2f", currencyAmount) + " is " + "$" + String.format("%.2f", (currencyAmount * 1.3)), Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e) {
+            Toast.makeText(this, "Enter a valid number", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
